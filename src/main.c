697c76f6ac3ce2911d16d01d32ae073e668aeaa3
Jachob Dolak <dolak.jachob@gmail.com>
2024-09-09 16:04:01 -0400

    added server mode

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
 #include <ncurses.h>
+#include <stdio.h>
+#include <strings.h>
+
 #include "base.h"
 
-int main(){
+int main( int argc, char *argv[] ){
+
+    if (argc > 1) {
+        if (!strcmp("--server", argv[1])) {
+            printf("Server Mode.\n");
+            return 0;
+        }
+    }
+
     start_screen();
     return 0;
 }
