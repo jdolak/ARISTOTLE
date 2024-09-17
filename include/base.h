207f4356734004c3196fbd90861ef9f68785933a
WAErdman <willaerdman@gmail.com>
2024-09-17 12:04:31 -0400

    include

--- include/base.h
+++ include/base.h
@@ -6,10 +6,11 @@
 #include <stdint.h>
 #include <sys/socket.h>
 #include <sys/errno.h>
-
+#include <stdlib.h>
 int start_screen(FILE *);
 int print_center(const char*, int, const char*);
 int basic_chat(WINDOW *);
 int menu(WINDOW *);
 int make_server(void);
 int telnet_naws(int, int *, int *);
+FILE* http_get(char*, char*);
